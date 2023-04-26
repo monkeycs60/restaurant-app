e13670e379951fa8e401ced8bdad65be5f3a7de8
monkeycs60 <clement.serizay@gmail.com>
2023-04-27 00:25:26 +0200

    🔍 chore(.gitignore): add .env file to gitignore 🎨 style(logo images): add new logo images to front-end assets The .env file is now added to the gitignore file, which prevents it from being committed to the repository. Two new logo images have been added to the front-end assets.
    🎨 style(HeroBanner.tsx): update logo image
    The logo image has been updated to a new version, which improves the visual appearance of the application. The old logo has been replaced with the new logo image.
    
    🎨 style(HeroBanner.tsx): increase logo size and adjust text styles
    The logo size has been increased to improve visibility and the text styles have been adjusted to improve readability and visual appeal. The first motion paragraph has been updated to reflect a new message, and the second motion paragraph has been updated to include a new span element. The third motion paragraph has been updated to include a new span element and adjust the text styles.
    
    💄 style(HeroBanner.tsx): improve hero banner button style
    The hero banner button has been updated to improve its style and user experience. The button is now centered and has a shadow effect when hovered over. The button text is now uppercase and the font size has been increased to improve readability.
    
    🎨 style(index.css): refactor CSS animations and add new gradient background animation
    This commit refactors the CSS animations by removing unused code and consolidating similar animations. A new gradient background animation has been added to the custom-animated-button class, which creates a smooth transition between three different opacity levels of the background color. The boxShadow class has been added to create a subtle shadow effect on elements.
    
    ✨ feat(fadeAnimations.css): add CSS animations for hero section
    This commit adds CSS animations for the hero section of the website. The translateHeroInfos animation moves the hero section to the right and back to the left. The fadeIn, fadeInTwo, and fadeInThree animations fade in different elements of the hero section. The fadeOut animation fades out an element and moves it to the left. The animations are triggered on hover and have different durations and timing functions.

--- front-end/src/components/Index/HeroBanner.tsx
+++ front-end/src/components/Index/HeroBanner.tsx
@@ -1,6 +1,5 @@
 import blindfold from '../../assets/blindfold2.png';
-import logo from '../../assets/logo2.png';
-import { useEffect, useState, useRef } from 'react';
+import logo from '../../assets/logo4.png';
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
 const HeroBanner = () => {
@@ -18,54 +17,54 @@ const HeroBanner = () => {
 					</div>
 				</div>
 				<button className='fixed right-12 mr-8'>
-					<img src={logo} alt='logo' className='w-24 cursor-pointer' />
+					<img src={logo} alt='logo' className='w-40 cursor-pointer' />
 				</button>
 			</div>
 			<div className='-mt-12 flex items-center justify-around xl:-mt-24 2xl:-mt-4'>
-				<div className='flex w-1/3 flex-col items-center justify-center gap-8 p-4'>
+				<div className='flex w-1/3 flex-col items-center justify-center gap-12 p-4'>
 					<motion.p
 						className={clsx(
-							'firstElement border-hero-text font-classic rounded-lg bg-amber-500/30 p-8 text-2xl leading-relaxed tracking-wide text-white',
+							'firstElement border-hero-text font-classic rounded-lg bg-amber-500/40 p-8 text-4xl leading-relaxed tracking-wide text-white',
 						)}
 						initial={{ opacity: 0, x: -100 }}
-						animate={{ opacity: 1, x: 5 }}
-						transition={{ duration: 2.3 }}
-					>
-						Une expérience unique à vivre au moins une fois dans sa vie,
-						êtes-vous prêts à sauter à l'aventure ?
-					</motion.p>
-					<motion.p
-						className={clsx(
-							'border-hero-text font-classic translate-x-2 rounded-lg p-8 text-2xl leading-relaxed tracking-wide text-white',
-						)}
-						initial={{ opacity: 0, x: 100 }}
-						animate={{ opacity: 1, x: -15 }}
+						animate={{ opacity: 1, x: 160 }}
 						transition={{ duration: 1.7 }}
 					>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Adipisci a vitae, excepturi soluta sed esse?
+						Ready for the{' '}
+						<span className='font-handwriting text-5xl'>
+							extraordinary?
+						</span>
 					</motion.p>
 					<motion.p
 						className={clsx(
-							'border-hero-text font-classic translate-x-8 rounded-lg p-8 text-2xl leading-relaxed tracking-wide text-white',
+							'border-hero-text font-classic translate-x-2 rounded-lg bg-amber-500/30 p-8 text-2xl leading-relaxed tracking-wide text-white',
 						)}
-						initial={{ opacity: 0, x: -100 }}
-						animate={{ opacity: 1, x: 35 }}
-						transition={{ duration: 2.1 }}
+						initial={{
+							opacity: 0,
+							x: 100,
+							backgroundColor: 'rgb(245 158 11 / 0.0)',
+						}}
+						animate={{
+							opacity: 1,
+							x: -15,
+							backgroundColor: 'rgb(245 158 11 / 0.4)',
+						}}
+						transition={{ duration: 1.3 }}
 					>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Adipisci a vitae, excepturi soluta sed esse?
+						Embark on an unprecedented{' '}
+						<span className='font-classic text-4xl underline underline-offset-4'>
+							culinary adventure
+						</span>{' '}
+						as you savor a meal in the captivating embrace of
 					</motion.p>
+
 					<motion.p
 						className={clsx(
-							'border-hero-text font-classic -translate-x-8 rounded-lg  p-8 text-2xl leading-relaxed tracking-wide text-white hover:opacity-100',
+							'font-classic fade-in-text border-hero-text translate-x-8 rounded-lg  p-16 text-5xl font-bold uppercase leading-relaxed tracking-widest text-white',
 						)}
-						initial={{ opacity: 0, x: 100 }}
-						animate={{ opacity: 1, x: 5 }}
-						transition={{ duration: 1.8 }}
 					>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Adipisci a vitae, excepturi soluta sed esse?
+						total
+						<span className='fade-in-span mx-6 text-6xl'>darkness</span>
 					</motion.p>
 				</div>
 				<div className='relative flex h-auto  w-1/3 flex-col'>
@@ -74,9 +73,11 @@ const HeroBanner = () => {
 						alt='femme yeux bandés'
 						className='z-0 w-[100%]'
 					/>
-					<button className='font-handwriting z-20 -mt-16 cursor-pointer rounded-lg bg-white px-[2vw] py-[1.5vh] text-2xl font-bold text-black hover:bg-black hover:text-white'>
-						Embark on a sensory journey
-					</button>
+					<div className='font-handwriting  relative -mt-20 flex w-full rounded-lg  border-yellow-50 text-4xl'>
+						<button className='boxShadow custom-animated-button z-20 w-full cursor-pointer rounded-lg px-[2vw] py-[2vh] font-bold  uppercase text-black shadow-md transition-all duration-300 ease-in-out hover:scale-110 hover:bg-black hover:text-white hover:shadow-lg '>
+							Embark on a sensory journey
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
