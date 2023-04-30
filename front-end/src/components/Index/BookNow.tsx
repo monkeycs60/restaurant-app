48741e413cfcf8cbd94d02a09206190afad03bfd
monkeycs60 <clement.serizay@gmail.com>
2023-04-30 16:11:50 +0200

    feature(booking): add book now section

--- front-end/src/components/Index/BookNow.tsx
+++ front-end/src/components/Index/BookNow.tsx
@@ -1,7 +1,15 @@
 const BookNow = () => {
 	return (
-		<div className="bg-red-500">
-			<p>Intrigued? Eager to learn more?</p>
+		<div className='font-classic mb-[5vh] mt-[2vh] flex w-full flex-col items-center gap-[4vh] text-center text-amber-400'>
+			<p className='text-base'>Intrigued? Eager to learn more?</p>
+			<p className='text-lg'>
+				Yearning to unravel these mysteries? Longing to embark on this
+				culinary adventure?
+			</p>
+			<p className='text-2xl'>Then</p>
+			<button className='w-1/4 rounded-lg bg-amber-500/80 px-[2vw] py-[2vh] text-3xl font-bold uppercase text-black hover:scale-105 hover:text-white'>
+				Book Now
+			</button>
 		</div>
 	);
 };
