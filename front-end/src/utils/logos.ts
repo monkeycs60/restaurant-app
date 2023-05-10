9b9028c0f5620dd5a0c244dafe6fe46742574fcd
monkeycs60 <clement.serizay@gmail.com>
2023-05-10 14:51:07 +0200

    fix: change logos logic

--- front-end/src/utils/logos.ts
+++ front-end/src/utils/logos.ts
@@ -10,19 +10,20 @@ import openFood from '../assets/featuredIn/openfood.png';
 import parisInsider from '../assets/featuredIn/logo_parisinsider_media_france.png';
 
 export interface LogoProps {
+	key: number;
 	src: string;
 	alt: string;
 }
 
 export const logos: LogoProps[] = [
-	{ src: bbcFood, alt: 'Logo 3' },
-	{ src: englishKitchen, alt: 'Logo 3' },
-	{ src: londonFoodLovers, alt: 'Logo 3' },
-	{ src: foodNetwork, alt: 'Logo 3' },
-	{ src: goodFood, alt: 'Logo 3' },
-	{ src: guardian, alt: 'Logo 3' },
-	{ src: Austin, alt: 'Logo 3' },
-	{ src: StripFood, alt: 'Logo 3' },
-	{ src: openFood, alt: 'Logo 3' },
-	{ src: parisInsider, alt: 'Logo 3' },
+	{ key: 0, src: bbcFood, alt: 'Logo 3' },
+	{ key: 1, src: englishKitchen, alt: 'Logo 3' },
+	{ key: 2, src: londonFoodLovers, alt: 'Logo 3' },
+	{ key: 3, src: foodNetwork, alt: 'Logo 3' },
+	{ key: 4, src: goodFood, alt: 'Logo 3' },
+	{ key: 5, src: guardian, alt: 'Logo 3' },
+	{ key: 6, src: Austin, alt: 'Logo 3' },
+	{ key: 7, src: StripFood, alt: 'Logo 3' },
+	{ key: 8, src: openFood, alt: 'Logo 3' },
+	{ key: 9, src: parisInsider, alt: 'Logo 3' },
 ];
