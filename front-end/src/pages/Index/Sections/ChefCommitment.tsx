55ca611f2bd379c9ce79fd9f4e7825788234bab9
monkeycs60 <clement.serizay@gmail.com>
2023-05-29 01:34:54 +0200

    responsive: full mobile

--- front-end/src/pages/Index/Sections/ChefCommitment.tsx
+++ front-end/src/pages/Index/Sections/ChefCommitment.tsx
@@ -19,7 +19,7 @@ const Team = () => {
 			<div
 				className={clsx(
 					' m-auto flex flex-col justify-between gap-12',
-					'lg:flex-row lg:gap-8 lg:p-6',
+					'lg:flex-row lg:gap-16 lg:p-6',
 					'3xl:p-0',
 				)}
 				ref={viewRef}
@@ -27,7 +27,7 @@ const Team = () => {
 				<div
 					className={clsx(
 						'm-auto flex w-[70%] rounded-lg border-2 border-white',
-						'lg:w-[45%] lg:border-none',
+						'lg:w-[50%] lg:border-none',
 					)}
 				>
 					<img
@@ -42,7 +42,7 @@ const Team = () => {
 				<div
 					className={clsx(
 						'concept-text-background flex flex-col gap-6 rounded-lg border-[1px] p-4 hover:bg-amber-600/30',
-						'lg:w-[45%] lg:gap-8 lg:p-8',
+						'lg:w-[50%] lg:gap-8 lg:p-8',
 						'3xl:gap-12',
 					)}
 				>
@@ -70,28 +70,33 @@ const Team = () => {
 					</div>
 				</div>
 			</div>
-			<section className='py-12'>
+			<section className='m-auto w-[90%] py-12 lg:w-auto'>
 				<div className='bg container mx-auto px-4'>
 					<div className='-mx-4 flex flex-wrap items-center'>
 						<div className='mb-12 w-full px-4 md:mb-16 lg:mb-0 lg:w-1/2'>
 							<div className='max-w-md'>
-								<h2 className='font-roboto mb-10 mt-8 text-4xl  text-gray-200'>
+								<h2 className='font-roboto mb-10 mt-8 text-xl text-gray-200  lg:text-4xl'>
 									We champion local and organic produce
 								</h2>
-								<p className='font-roboto text-lg text-gray-500'>
+								<p className='font-roboto text-sm text-gray-500 lg:text-lg'>
 									We take pride in our commitment to quality with all
 									our meats, fruits and vegetables being sustainably
 									sourced and certified.
 								</p>
 							</div>
 						</div>
-						<div className='flex w-full translate-x-6 rounded-lg px-28 py-12 lg:w-1/2'>
+						<div
+							className={clsx(
+								'flex w-full  rounded-lg px-8 py-6 ',
+								'lg:w-1/2 lg:translate-x-6 lg:px-28 lg:py-12',
+							)}
+						>
 							<div className='-m-4 flex flex-wrap'>
 								{logosPhotos.map((logo) => (
-									<div key={logo.key} className='w-1/2 p-2 md:w-1/3'>
+									<div key={logo.key} className='w-1/3 p-2 lg:w-1/3'>
 										<div className='rounded-lg bg-zinc-300 py-4 grayscale hover:grayscale-0 lg:py-2'>
 											<img
-												className='mx-auto h-20 w-20 object-cover'
+												className='mx-auto h-12 w-12 object-cover lg:h-20 lg:w-20'
 												src={logo.src}
 												alt=''
 											/>
@@ -105,7 +110,8 @@ const Team = () => {
 			</section>
 			<div
 				className={clsx(
-					'TRANSITION font-roboto flex w-full justify-center gap-6 text-3xl font-extralight uppercase tracking-wide text-gray-200 ',
+					'TRANSITION font-roboto m-auto  flex w-[80%] justify-center gap-6 text-center text-2xl font-extralight uppercase tracking-wide text-gray-200',
+					'lg:w-full lg:text-left lg:text-3xl',
 					'3xl:text-4xl 3xl:my-[5vh]',
 				)}
 			>
--- front-end/src/pages/Index/Sections/PracticalInformations.tsx
+++ front-end/src/pages/Index/Sections/PracticalInformations.tsx
@@ -10,86 +10,87 @@ import 'leaflet/dist/leaflet.css';
 import { RiContactsBook2Line } from 'react-icons/ri';
 import { GoLocation } from 'react-icons/go';
 import { BsCalendarDay } from 'react-icons/bs';
+import clsx from 'clsx';
 const PracticalInformations = () => {
 	return (
-		<section className='3xl:py-2 3xl:pb-10 relative overflow-hidden py-10 md:py-12'>
+		<section className='3xl:py-2 3xl:pb-10 relative m-auto w-[85%] overflow-hidden py-10 md:py-12 lg:w-auto'>
 			<div className='container relative mx-auto px-4'>
 				<div className='mx-auto max-w-7xl'>
-					<div className='mx-auto mb-20 flex max-w-2xl flex-col gap-4 text-center'>
-						<h1 className='font-playfair xs:text-6xl mb-4 text-5xl font-bold text-gray-100'>
+					<div className='mx-auto mb-10 flex max-w-2xl flex-col gap-4 text-center lg:mb-20'>
+						<h1 className='font-playfair mb-4 text-2xl font-bold text-gray-100 lg:text-5xl'>
 							<span>Coming to </span>{' '}
-							<span className='font-dancing font-serif text-6xl text-orange-600'>
+							<span className='font-dancing block font-serif text-4xl text-orange-600 lg:inline lg:text-6xl'>
 								Chiaroscuro
 							</span>
 						</h1>
-						<p className='font-roboto text-xl text-gray-300'>
+						<p className='font-roboto text-gray-300 lg:text-xl'>
 							Our team eagerly awaits your visit, whether it's during the
 							weekday or over the weekend, ready to reawaken your senses.
 						</p>
 					</div>
-					<div className='mb-12 flex justify-between'>
-						<div className='xs:max-w-sm concept-text-background font-roboto mx-auto w-1/2 rounded-lg p-4 lg:max-w-[50%]'>
-							<div className='flex flex-col flex-wrap items-center gap-8 p-6'>
+					<div className='flex flex-col justify-between lg:mb-12 lg:flex-row'>
+						<div className='xs:max-w-sm concept-text-background font-roboto mx-auto rounded-lg p-4 lg:w-1/2 lg:max-w-[50%]'>
+							<div className='flex flex-col flex-wrap items-center gap-6 p-2 lg:gap-8 lg:p-6'>
 								<div className='w-full px-4'>
 									<div className='flex items-center gap-6'>
-										<div className=' flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 sm:h-14 sm:w-14'>
+										<div className=' flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 lg:h-14 lg:w-14'>
 											<RiContactsBook2Line
-												className='h-5 w-5'
+												className='h-3 w-3 lg:h-5 lg:w-5'
 												color='white'
 											/>
 										</div>
 										<div className='flex flex-col'>
-											<span className='text-base text-gray-500'>
+											<span className='text-sm text-gray-500 lg:text-base'>
 												Contact
 											</span>
-											<span className='block text-base font-semibold text-gray-900'>
+											<span className='block text-sm font-semibold text-gray-900 lg:text-base'>
 												020 2831 9278
 											</span>
-											<span className='block text-base font-semibold text-gray-900'>
+											<span className='block text-sm font-semibold text-gray-900 lg:text-base'>
 												chiaroscuro@contact.com
 											</span>
 										</div>
 									</div>
 								</div>
 								<div className='w-full px-4'>
-									<div className='flex items-start gap-6'>
-										<div className='mr-5 flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 sm:h-14 sm:w-14'>
+									<div className='flex items-start lg:gap-6'>
+										<div className='mr-5 flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 lg:h-14 lg:w-14'>
 											<GoLocation
-												className='h-5 w-5'
+												className='h-3 w-3 lg:h-5 lg:w-5'
 												color='white'
 											/>
 										</div>
 										<div>
-											<span className='text-base text-gray-500'>
+											<span className='text-sm text-gray-500 lg:text-base'>
 												Location
 											</span>
-											<span className='block text-base font-semibold text-gray-900'>
+											<span className='block text-sm font-semibold text-gray-900 lg:text-base'>
 												10 Shaftesbury Avenue,
 											</span>
-											<span className='block text-base font-semibold text-gray-900'>
+											<span className='block text-sm font-semibold text-gray-900 lg:text-base'>
 												London
 											</span>
 										</div>
 									</div>
 								</div>
 								<div className='w-full px-4'>
-									<div className='flex items-center gap-6'>
-										<div className='mr-5 flex h-6 w-6 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 sm:h-14 sm:w-14'>
+									<div className='flex items-center lg:gap-6'>
+										<div className='mr-5 flex h-8 w-8 shrink-0 items-center justify-center rounded-full bg-gray-700 p-1 sm:mr-8 lg:h-14 lg:w-14'>
 											<BsCalendarDay
-												className='h-5 w-5'
+												className='h-3 w-3 lg:h-5 lg:w-5'
 												color='white'
 											/>
 										</div>
 										<div>
-											<span className='text-base text-gray-500'>
+											<span className='hidden text-sm text-gray-500 lg:block lg:text-base'>
 												Opening Hours
 											</span>
 											<div className='flex w-full  items-center gap-8'>
 												<div className='flex flex-col justify-center'>
-													<span className='block text-base font-semibold text-gray-900'>
+													<span className='block text-sm font-semibold text-gray-900 lg:text-base'>
 														Tuesday-Saturday
 													</span>
-													<span className='block text-sm text-gray-900'>
+													<span className='block text-xs text-gray-900 lg:text-sm'>
 														12AM - 2PM / 7PM - 11PM
 													</span>
 												</div>
@@ -107,7 +108,12 @@ const PracticalInformations = () => {
 								</div>
 							</div>
 						</div>
-						<div className='rounded-4xl flex w-[35%] -translate-x-48 justify-center overflow-hidden rounded-lg'>
+						<div
+							className={clsx(
+								'rounded-4xl flex h-72 justify-center overflow-hidden rounded-lg',
+								'lg:h-auto lg:w-[35%] lg:-translate-x-48',
+							)}
+						>
 							<MapContainer
 								center={[51.510607, -0.134301]}
 								zoom={14}
