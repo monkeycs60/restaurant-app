392ba2b8bbe38b97b8f1711ca7667402812d0a1e
monkeycs60 <clement.serizay@gmail.com>
2023-05-30 11:52:06 +0200

    refactor: api service

--- front-end/src/hooks/useAuth.tsx
+++ front-end/src/hooks/useAuth.tsx
@@ -1,18 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import { authenticate } from '../services/api';
 
 const useAuth = <Tipo,>(endpoint: string) => {
-	const API = axios.create({
-		baseURL: 'http://localhost:3001',
-	});
-	// Define your authentication function
-	const authenticate = async (credentials: Tipo) => {
-		const response = await API.post(`/auth/${endpoint}`, credentials);
-		return response.data;
-	};
-
 	// Use the useMutation hook with the authenticate function
-	const authMutation = useMutation(authenticate);
+	const authMutation = useMutation((credentials: Tipo) =>
+		authenticate(endpoint, credentials),
+	);
 
 	// Return the authMutation object from the custom hook
 	return authMutation;
--- front-end/src/services/api.ts
+++ front-end/src/services/api.ts
@@ -1,9 +1,26 @@
 import axios from 'axios';
 import { BookingFormData } from '../hooks/useBooking';
 
+const isLocal = false;
+
+const API_URL_RAILWAY = 'restaurant-app-production-29bf.up.railway.app';
+const API_URL_LOCAL = 'http://localhost:3001';
+
+isLocal
+	? (axios.defaults.baseURL = API_URL_LOCAL)
+	: (axios.defaults.baseURL = API_URL_RAILWAY);
+
+export async function authenticate<Tipo>(endpoint: string, credentials: Tipo) {
+	try {
+		const response = await axios.post(`/auth/${endpoint}`, credentials);
+		return response.data;
+	} catch (error) {
+		throw new Error(`Error authenticating: ${error}`);
+	}
+}
 export async function fetchBookedDates() {
 	try {
-		const response = await axios.get('http://localhost:3001/booking');
+		const response = await axios.get('/booking');
 		return response.data.map(
 			(booking: BookingFormData) => new Date(booking.date),
 		);
@@ -13,9 +30,7 @@ export async function fetchBookedDates() {
 }
 export async function fetchBookedDate(bookingId: string) {
 	try {
-		const response = await axios.get(
-			`http://localhost:3001/booking/${bookingId}`,
-		);
+		const response = await axios.get(`/booking/${bookingId}`);
 		return response.data;
 	} catch (error) {
 		throw new Error('Error fetching booked date');
