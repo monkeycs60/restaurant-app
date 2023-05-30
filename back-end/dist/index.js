54294206ac107d90c7ee92e8381bbd0c337b504b
monkeycs60 <clement.serizay@gmail.com>
2023-05-30 23:16:59 +0200

    fix: change CORS policy

--- back-end/dist/index.js
+++ back-end/dist/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { usersRouter } from './routes/users.js';
@@ -22,11 +21,13 @@ const app = express();
 // 	},
 // 	credentials: true,
 // };
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-};
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 app.use(express.json());
 app.use('/auth', usersRouter);
 app.use('/recipes', recipesRouter);
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -27,11 +27,20 @@ const app = express();
 // 	},
 // 	credentials: true,
 // };
-const corsOptions = {
-	origin: '*',
-	credentials: true,
-};
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req: any, res: any, next: any) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+	);
+	next();
+});
 
 app.use(express.json());
 
